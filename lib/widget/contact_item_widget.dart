19df4bae1238f365d5d4804d887d29f0ed54d607
lei.chang <540739522@qq.com>
2020-03-29 21:20:51 +0800

    修改主题

--- lib/widget/contact_item_widget.dart
+++ lib/widget/contact_item_widget.dart
@@ -13,56 +13,75 @@ import 'package:flutter/cupertino.dart';
 /// 联系人列表item
 class ContactItemWidget extends StatelessWidget {
   final Contact contact;
+  final double height;
 
   const ContactItemWidget({
     Key key,
     @required this.contact,
+    @required this.height,
   })  : assert(contact != null),
+        assert(height != null),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return WidgetGroup.spacing(
-      alignment: MainAxisAlignment.start,
-      spacing: 10,
-      children: <Widget>[
-        CupertinoCircleAvatar.memory(
-          bytes: contact.avatar,
-          assetName: Images.ic_default_avatar,
-          size: 65,
-        ),
-        Expanded(
-          child: WidgetGroup.spacing(
-            alignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            direction: Axis.vertical,
-            spacing: 8,
-            children: <Widget>[
-              Text(
-                contact.displayName ?? '',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: CupertinoDynamicColor.resolve(
-                    CupertinoColors.label,
-                    context,
-                  ),
-                ),
-              ),
-              Text(
-                (contact.phones == null || contact.phones.isEmpty ? null : contact.phones.first.value) ?? '',
-                style: TextStyle(
-                  fontSize: 15,
-                  color: CupertinoDynamicColor.resolve(
-                    CupertinoColors.secondaryLabel,
-                    context,
-                  ),
-                ),
-              ),
-            ],
+    var phones = contact.phones;
+    var phone;
+    if (phones != null && phones.isNotEmpty) {
+      phone = phones.first.value;
+    }
+    return Container(
+      height: height,
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
+      color: CupertinoDynamicColor.resolve(
+        CupertinoTheme.of(context).barBackgroundColor,
+        context,
+      ),
+      child: WidgetGroup.spacing(
+        alignment: MainAxisAlignment.start,
+        spacing: 10,
+        children: <Widget>[
+          CupertinoCircleAvatar.memory(
+            bytes: contact.avatar,
+            assetName: Images.ic_default_avatar,
+            size: 65,
           ),
-        ),
-      ],
+          Expanded(
+            child: WidgetGroup.spacing(
+              alignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              direction: Axis.vertical,
+              spacing: 8,
+              children: <Widget>[
+                Text(
+                  contact.displayName ?? '',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: CupertinoDynamicColor.resolve(
+                      CupertinoColors.label,
+                      context,
+                    ),
+                  ),
+                ),
+                Text(
+                  phone ?? '',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: CupertinoDynamicColor.resolve(
+                      CupertinoColors.secondaryLabel,
+                      context,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
