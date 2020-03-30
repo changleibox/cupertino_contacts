07865306c29395e4a8ae7ad53911126fd879d58e
lei.chang <540739522@qq.com>
2020-03-30 19:19:41 +0800

    优化

--- lib/widget/activity_indicator.dart
+++ lib/widget/activity_indicator.dart
@@ -26,9 +26,11 @@ class SupportCupertinoActivityIndicator extends StatefulWidget {
     Key key,
     this.animating = true,
     this.radius = _kDefaultIndicatorRadius,
+    this.position,
   })  : assert(animating != null),
         assert(radius != null),
         assert(radius > 0),
+        assert(position == null || position >= 0.0 && position <= 1.0),
         super(key: key);
 
   /// Whether the activity indicator is running its animation.
@@ -40,6 +42,9 @@ class SupportCupertinoActivityIndicator extends StatefulWidget {
   ///
   /// Defaults to 10px. Must be positive and cannot be null.
   final double radius;
+
+  /// [0.0-1.0]
+  final double position;
 
   @override
   _SupportCupertinoActivityIndicatorState createState() => _SupportCupertinoActivityIndicatorState();
@@ -86,6 +91,7 @@ class _SupportCupertinoActivityIndicatorState extends State<SupportCupertinoActi
           position: _controller,
           activeColor: CupertinoDynamicColor.resolve(_kActiveTickColor, context),
           radius: widget.radius,
+          progress: widget.animating ? null : widget.position,
         ),
       ),
     );
@@ -104,6 +110,7 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
     @required this.position,
     @required this.activeColor,
     double radius,
+    @required this.progress,
   })  : tickFundamentalRRect = RRect.fromLTRBXY(
           -radius,
           radius / _kDefaultIndicatorRadius,
@@ -117,6 +124,7 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
   final Animation<double> position;
   final RRect tickFundamentalRRect;
   final Color activeColor;
+  final double progress;
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -125,11 +133,15 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
     canvas.save();
     canvas.translate(size.width / 2.0, size.height / 2.0);
 
-    final int activeTick = (_kTickCount * position.value).floor();
+    final int activeTick = (_kTickCount * (progress ?? position.value)).floor();
 
     for (int i = 0; i < _kTickCount; ++i) {
       final int t = (i + activeTick) % _kTickCount;
-      paint.color = activeColor.withAlpha(_alphaValues[t]);
+      int alpha = _alphaValues[t];
+      if (progress != null) {
+        alpha = (_kTickCount - i) % _kTickCount <= activeTick ? 255 : 0;
+      }
+      paint.color = activeColor.withAlpha(alpha);
       canvas.drawRRect(tickFundamentalRRect, paint);
       canvas.rotate(-_kTwoPI / _kTickCount);
     }
@@ -139,6 +151,6 @@ class _CupertinoActivityIndicatorPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(_CupertinoActivityIndicatorPainter oldPainter) {
-    return oldPainter.position != position || oldPainter.activeColor != activeColor;
+    return oldPainter.position != position || oldPainter.activeColor != activeColor || oldPainter.progress != progress;
   }
 }
