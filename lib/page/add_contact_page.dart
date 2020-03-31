ffa2418e7786718ecdb0a26181eae532903d72b5
lei.chang <540739522@qq.com>
2020-03-31 21:33:10 +0800

    新建联系人

--- lib/page/add_contact_page.dart
+++ lib/page/add_contact_page.dart
@@ -2,6 +2,8 @@
  * Copyright (c) 2020 CHANGLEI. All rights reserved.
  */
 
+import 'dart:ui';
+
 import 'package:cupertinocontacts/resource/colors.dart';
 import 'package:cupertinocontacts/widget/add_contact_persistent_header_delegate.dart';
 import 'package:cupertinocontacts/widget/cupertino_divider.dart';
@@ -166,8 +168,80 @@ class _AddContactPageState extends State<AddContactPage> {
                 alignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 direction: Axis.vertical,
-                itemCount: 1,
+                itemCount: 11,
                 itemBuilder: (context, index) {
+                  if (index < 10) {
+                    return Container(
+                      padding: EdgeInsets.only(
+                        left: 16,
+                        right: 10,
+                      ),
+                      child: WidgetGroup.spacing(
+                        spacing: 10,
+                        children: [
+                          CupertinoButton(
+                            minSize: 44,
+                            borderRadius: BorderRadius.zero,
+                            padding: EdgeInsets.zero,
+                            child: WidgetGroup.spacing(
+                              spacing: 10,
+                              children: [
+                                Icon(
+                                  CupertinoIcons.minus_circled,
+                                  color: CupertinoColors.systemRed,
+                                ),
+                                Text(
+                                  '住宅',
+                                  style: CupertinoTheme.of(context).textTheme.textStyle,
+                                ),
+                                Icon(
+                                  CupertinoIcons.forward,
+                                  color: CupertinoDynamicColor.resolve(
+                                    CupertinoColors.secondaryLabel,
+                                    context,
+                                  ),
+                                ),
+                                Container(
+                                  width: 0.5,
+                                  height: 44,
+                                  color: CupertinoDynamicColor.resolve(
+                                    separatorColor.withOpacity(0.1),
+                                    context,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            onPressed: () {},
+                          ),
+                          Expanded(
+                            child: SizedBox(
+                              height: 44,
+                              child: CupertinoTextField(
+                                placeholder: '姓氏',
+                                placeholderStyle: TextStyle(
+                                  color: CupertinoDynamicColor.resolve(
+                                    placeholderColor,
+                                    context,
+                                  ),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: CupertinoColors.tertiarySystemBackground,
+                                ),
+                                padding: EdgeInsets.only(
+                                  left: 10,
+                                  right: 10,
+                                ),
+                                clearButtonMode: OverlayVisibilityMode.editing,
+                                scrollPadding: EdgeInsets.only(
+                                  bottom: 54,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
                   return CupertinoButton(
                     minSize: 44,
                     padding: EdgeInsets.only(
