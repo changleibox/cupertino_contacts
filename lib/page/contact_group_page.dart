f9b21177f9534a7cc40050f36f65f24c8fddf96d
lei.chang <540739522@qq.com>
2020-04-01 12:38:57 +0800

    修改群组页面，增加下拉刷新

--- lib/page/contact_group_page.dart
+++ lib/page/contact_group_page.dart
@@ -2,8 +2,11 @@
  * Copyright (c) 2020 CHANGLEI. All rights reserved.
  */
 
+import 'package:cupertinocontacts/presenter/contact_group_presenter.dart';
 import 'package:cupertinocontacts/resource/colors.dart';
 import 'package:cupertinocontacts/widget/cupertino_divider.dart';
+import 'package:cupertinocontacts/widget/framework.dart';
+import 'package:cupertinocontacts/widget/support_refresh_indicator.dart';
 import 'package:cupertinocontacts/widget/widget_group.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -17,15 +20,11 @@ class ContactGroupPage extends StatefulWidget {
   _ContactGroupPageState createState() => _ContactGroupPageState();
 }
 
-class _ContactGroupPageState extends State<ContactGroupPage> {
-  final _items = [
-    '所有"iPhone"',
-    'Friends',
-    'Work',
-  ];
+class _ContactGroupPageState extends PresenterState<ContactGroupPage, ContactGroupPresenter> {
+  _ContactGroupPageState() : super(ContactGroupPresenter());
 
   @override
-  Widget build(BuildContext context) {
+  Widget builds(BuildContext context) {
     return CupertinoPageScaffold(
       backgroundColor: CupertinoColors.secondarySystemBackground,
       navigationBar: CupertinoNavigationBar(
@@ -42,80 +41,80 @@ class _ContactGroupPageState extends State<ContactGroupPage> {
           },
         ),
       ),
-      child: ListView(
-        children: <Widget>[
-          WidgetGroup(
-            alignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            direction: Axis.vertical,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 16,
-                  top: 32,
-                  right: 16,
-                  bottom: 4,
-                ),
-                child: Text(
-                  'IPHONE',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: CupertinoDynamicColor.resolve(
-                      CupertinoColors.secondaryLabel,
-                      context,
-                    ),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SupportSliverRefreshIndicator(
+            onRefresh: presenter.onRefresh,
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(
+              left: 16,
+              top: 32,
+              right: 16,
+              bottom: 4,
+            ),
+            sliver: SliverToBoxAdapter(
+              child: Text(
+                'IPHONE',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: CupertinoDynamicColor.resolve(
+                    CupertinoColors.secondaryLabel,
+                    context,
                   ),
                 ),
               ),
-              Container(
-                color: CupertinoDynamicColor.resolve(
-                  CupertinoColors.tertiarySystemBackground,
-                  context,
-                ),
-                child: WidgetGroup.separated(
-                  alignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  direction: Axis.vertical,
-                  itemCount: _items.length,
-                  itemBuilder: (context, index) {
-                    return CupertinoButton(
-                      child: WidgetGroup.spacing(
-                        alignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            _items[index],
-                            style: CupertinoTheme.of(context).textTheme.textStyle,
-                          ),
-                          Icon(
-                            CupertinoIcons.check_mark,
-                            size: 40,
-                            color: CupertinoTheme.of(context).primaryColor,
-                          ),
-                        ],
-                      ),
-                      padding: EdgeInsets.only(
-                        left: 16,
-                        right: 8,
-                      ),
-                      color: CupertinoColors.tertiarySystemBackground,
-                      minSize: 44,
-                      borderRadius: BorderRadius.zero,
-                      onPressed: () {},
-                    );
-                  },
-                  separatorBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.only(
-                        left: 16,
-                      ),
-                      child: CupertinoDivider(
-                        color: separatorColor.withOpacity(0.2),
-                      ),
-                    );
-                  },
-                ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              color: CupertinoDynamicColor.resolve(
+                CupertinoColors.tertiarySystemBackground,
+                context,
               ),
-            ],
+              child: WidgetGroup.separated(
+                alignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                direction: Axis.vertical,
+                itemCount: presenter.items.length,
+                itemBuilder: (context, index) {
+                  return CupertinoButton(
+                    child: WidgetGroup.spacing(
+                      alignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          presenter.items[index],
+                          style: CupertinoTheme.of(context).textTheme.textStyle,
+                        ),
+                        Icon(
+                          CupertinoIcons.check_mark,
+                          size: 40,
+                          color: CupertinoTheme.of(context).primaryColor,
+                        ),
+                      ],
+                    ),
+                    padding: EdgeInsets.only(
+                      left: 16,
+                      right: 8,
+                    ),
+                    color: CupertinoColors.tertiarySystemBackground,
+                    minSize: 44,
+                    borderRadius: BorderRadius.zero,
+                    onPressed: () {},
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: 16,
+                    ),
+                    child: CupertinoDivider(
+                      color: separatorColor.withOpacity(0.2),
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
         ],
       ),
---
+++ lib/presenter/contact_group_presenter.dart
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 CHANGLEI. All rights reserved.
+ */
+
+import 'package:cupertinocontacts/page/contact_group_page.dart';
+import 'package:cupertinocontacts/presenter/presenter.dart';
+
+class ContactGroupPresenter extends Presenter<ContactGroupPage> {
+  final _items = List<String>();
+
+  List<String> get items => List.unmodifiable(_items);
+
+  Future<void> onRefresh() async {
+    _items.clear();
+    _items.addAll([
+      '所有"iPhone"',
+      'Friends',
+      'Work',
+    ]);
+    notifyDataSetChanged();
+  }
+}
