c3f3484998305388be5eb80b34dc4b2c8f1e3c76
lei.chang <540739522@qq.com>
2020-04-01 16:30:09 +0800

    新建联系人

---
+++ lib/constant/selection.dart
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2020 CHANGLEI. All rights reserved.
+ */
+
+const phoneSelections = [
+  '住宅',
+  '工作',
+  '学校',
+  'iPhone',
+  '手机',
+  '主要',
+  '家庭传真',
+  '工作传真',
+  '传呼机',
+  '其他',
+];
+const emailSelections = [
+  '住宅',
+  '工作',
+  '学校',
+  'iCloud',
+  '其他',
+];
+const urlSelections = [
+  '主页',
+  '住宅',
+  '工作',
+  '学校',
+  '其他',
+];
+const addressSelections = [
+  '住宅',
+  '工作',
+  '学校',
+  '其他',
+];
+const birthdaySelections = [
+  '默认生日',
+  '农历生日',
+  '希伯来历',
+  '伊斯兰厉',
+];
+const dateSelections = [
+  '纪念日',
+  '其他',
+];
+const relatedPartySelections = [
+  '父母',
+  '兄弟',
+  '姐妹',
+  '子女',
+  '配偶',
+  '助理',
+  '父亲',
+  '母亲',
+  '哥哥',
+  '姐姐',
+  '弟弟',
+  '妹妹',
+  '丈夫',
+  '妻子',
+  '伴侣',
+  '儿子',
+  '女儿',
+  '朋友',
+  '上司',
+  '同事',
+  '其他',
+  '所有标签',
+];
+const socialDataSelections = [
+  'Twitter',
+  'Facebook',
+  'Flickr',
+  '领英',
+  'Myspace',
+  '新浪微博',
+];
+const instantMessagingSelections = [
+  'Skype',
+  'MSN',
+  'Google Talk',
+  'AIM',
+  '雅虎',
+  'ICQ',
+  'Jabber',
+  'QQ',
+  'Gadu-Gadu',
+];
