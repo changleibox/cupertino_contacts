b3a798b7641f88590399efab37ef2deacd23237e
lei.chang <540739522@qq.com>
2020-04-02 16:07:29 +0800

    新建联系人

--- lib/presenter/add_contact_presenter.dart
+++ lib/presenter/add_contact_presenter.dart
@@ -123,6 +123,7 @@ class AddContactPresenter extends Presenter<AddContactPage> implements ValueList
       }
       avatar = value;
       notifyDataSetChanged();
+      notifyListeners();
     });
   }
 
