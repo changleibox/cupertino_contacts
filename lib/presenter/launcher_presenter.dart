27342eacfd65d6f4c048709d5348546d04515f49
lei.chang <540739522@qq.com>
2020-04-10 18:52:21 +0800

    优化

--- lib/presenter/launcher_presenter.dart
+++ lib/presenter/launcher_presenter.dart
@@ -2,31 +2,63 @@
  * Copyright (c) 2020 CHANGLEI. All rights reserved.
  */
 
+import 'dart:async';
+
 import 'package:cupertinocontacts/page/launcher_page.dart';
 import 'package:cupertinocontacts/presenter/presenter.dart';
 import 'package:cupertinocontacts/route/route_provider.dart';
-import 'package:cupertinocontacts/widget/toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class LauncherPresenter extends Presenter<LauncherPage> {
+  Timer _timer;
+
   @override
   void postFrameCallback() {
-    _requestPermission().then((value) {
-      Navigator.pushNamedAndRemoveUntil(context, RouteProvider.home, (route) => false);
-    }).catchError((_) {
-      showText('没有操作权限', context);
+    _timer = Timer(Duration(seconds: 2), () {
+      _requestPermission().then((value) {
+        Navigator.pushNamedAndRemoveUntil(context, RouteProvider.home, (route) => false);
+      }).catchError((_) async {
+        _showNotPermissionDialog();
+      });
     });
     super.postFrameCallback();
   }
 
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   Future<void> _requestPermission() async {
-    await Future.delayed(Duration(seconds: 3));
     var permissionHandler = PermissionHandler();
     await permissionHandler.requestPermissions([PermissionGroup.contacts]);
     var permissionStatus = await permissionHandler.checkPermissionStatus(PermissionGroup.contacts);
     if (permissionStatus != PermissionStatus.granted) {
       return Future.error('Permission denial.');
     }
+  }
+
+  Future<void> _showNotPermissionDialog() {
+    return showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: Text('提示'),
+          content: Text('没有通讯录权限，请在设置里授权，才能正常使用'),
+          actions: [
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              child: Text('去设置'),
+              onPressed: () {
+                Navigator.pop(context);
+                PermissionHandler().openAppSettings();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
